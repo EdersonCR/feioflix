7d6ad5aa319f086436fef2d892317c02d3249e27
EdersonCR <edersonrodrigues10@hotmail.com>
2020-07-28 01:28:15 -0300

    Setup inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div style={{background: "var(--black)"}}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Em 2016 o guitarrista David Gilmour se apresentou na lendária cidade de Pompeia, na Itália. O show marcou o retorno do guitarrista ao mesmo local onde, 45 anos antes, o Pink Floyd fez história ao se apresentar e lançar o histórico Live At Pompeii."}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />           
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
